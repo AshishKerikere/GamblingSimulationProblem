504cc7dfe0fd16f4de0facd73fba08da9a34ca19
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-07 15:44:56 +0530

    Gambling Simulation UC1

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,1 +1,9 @@
-
+public class GamblingSimulation {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter Stakes ");
+                int stakes = scanner.nextInt();
+                System.out.println("Enter the Bet Amount");
+                int betAmount = scanner.nextInt();
+    }
+}
