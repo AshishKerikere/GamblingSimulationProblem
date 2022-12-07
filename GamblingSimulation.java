cc720c05310f4c8ba6fd8dddfe9aec440d61fc4f
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-12-07 15:50:07 +0530

    GamblingSimulation UC3

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -6,9 +6,12 @@ public class GamblingSimulation {
         int stakes = scanner.nextInt();
         System.out.println("Enter the Bet Amount");
         int betAmount = scanner.nextInt();
+        System.out.println("Enter the Percentage of Swing from 1 to 100");
+        int swingPercentage = scanner.nextInt();
 
-        stakes = bettingResult(stakes, betAmount);
-        System.out.println("The resultant stakes after one round of betting " +stakes);
+        int maxSwing = (int)((swingPercentage*1.0/100.0)*stakes);
+
+        int resultantValueAtTheEndOfDay =  bettingDayResult(stakes, betAmount, maxSwing);
 
     }
 
@@ -26,5 +29,28 @@ public class GamblingSimulation {
                 break;
         }
         return stakes;
+    }
+
+    public static int bettingDayResult(int stakes, int betAmount, int maxSwing){
+        int upSwing = 0;
+        int downSwing = 0;
+
+        int currentHoldingValue = stakes;
+        int previousHoldingValue;
+
+        while (upSwing < maxSwing && downSwing < maxSwing ){
+            previousHoldingValue = currentHoldingValue;
+            currentHoldingValue = bettingResult(previousHoldingValue, betAmount);
+            if (currentHoldingValue > previousHoldingValue){
+                upSwing++;
+            }
+            else {
+                downSwing++;
+            }
+        }
+        System.out.println("Upswing is = " +upSwing);
+        System.out.println("Downswing is = " +downSwing);
+        System.out.println("Current Holding Value is = " +currentHoldingValue);
+        return currentHoldingValue;
     }
 }
